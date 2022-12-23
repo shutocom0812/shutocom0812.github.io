35c1ff49ca2487b161c8b6d58134ae4e3e6981f2
shutocom0812 <116039772+shutocom0812@users.noreply.github.com>
2022-12-23 14:25:24 +0900

    Update theme.js

--- resources/theme.js
+++ resources/theme.js
@@ -1,17 +1,20 @@
-let themeIndex = 0;
+		let themeIndex = 0;
 let themes = [
-  ["#E5996D", "#642327", "#AD4133"],
-  ["#642327", "#E5996D", "#AD4133"],
   ["#021e1e", "#E4DAD9", "#e1a600"],
   ["#E4DAD9", "#021e1e", "#e1a600"],
   ["#473857", "#E8C9C7", "#149684"],
   ["#E8C9C7", "#473857", "#149684"],
-  ["#bad2ba", "#092837", "#d28859"]
+  ["#005051", "#fffafc", "#95bf43"],
+  ["#fffafc", "#005051", "#95bf43"],
+	["#005051", "#fcf6cc", "#a4b059"],
+	["#fcf6cc", "#005051", "#a4b059"],
+	["#005051", "#fefafb", "#909668"],
+	["#fefafb", "#005051", "#909668"]
 ];
 const Root = document.querySelector(":root");
 
 function themeFirst() {
-  const nowValue = localStorage.getItem("theme");
+  const nowValue = Number(localStorage.getItem("theme"));
   if (nowValue != null) {
     themes[nowValue].forEach((color, index) => Root.style.setProperty(`--color${index + 1}`, color));
     themeIndex = nowValue;
@@ -27,17 +30,15 @@ function themeButton() {
   Button.name = "theme";
   Button.classList.add("theme-button");
   Button.tabIndex = -1;
+	Button.innerText = themeIndex;
   Button.addEventListener("click", function(){themeChange();}, false);
-  document.getElementById("headerNav").appendChild(Button);
+  document.querySelector("body").appendChild(Button);
 }
 
 function themeChange() {
-  if (themeIndex >= themes.length - 1) {
-    themeIndex = 0;
-  } else {
-    themeIndex++;
-  }
+	themeIndex = (themeIndex >= themes.length - 1) ? 0 : themeIndex + 1;
   themes[themeIndex].forEach((color, index) => Root.style.setProperty(`--color${index + 1}`, color));
+	document.getElementById("themeButton").innerText = themeIndex;
   localStorage.setItem("theme", themeIndex);
   console.log(themeIndex);
 }
