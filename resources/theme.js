bc2170ba5580fff4ea61f3827679200f5a80c16c
shutocom0812 <116039772+shutocom0812@users.noreply.github.com>
2022-12-24 02:15:50 +0900

    Update theme.js

--- resources/theme.js
+++ resources/theme.js
@@ -1,15 +1,13 @@
-		let themeIndex = 0;
+let themeIndex = 0;
 let themes = [
-  ["#021e1e", "#E4DAD9", "#e1a600"],
-  ["#E4DAD9", "#021e1e", "#e1a600"],
-  ["#473857", "#E8C9C7", "#149684"],
-  ["#E8C9C7", "#473857", "#149684"],
-  ["#005051", "#fffafc", "#95bf43"],
-  ["#fffafc", "#005051", "#95bf43"],
-	["#005051", "#fcf6cc", "#a4b059"],
-	["#fcf6cc", "#005051", "#a4b059"],
-	["#005051", "#fefafb", "#909668"],
-	["#fefafb", "#005051", "#909668"]
+  ["#021e1e", "#e4dad9", "#e1a600"],	//Tokyo-Light
+  ["#e4dad9", "#021e1e", "#e1a600"],	//Tokyo-Dark
+  ["#473857", "#e8c9c7", "#149684"],	//Kure-Light
+  ["#e8c9c7", "#473857", "#149684"],	//Kure-Dark
+  ["#005051", "#fffafc", "#95bf43"],	//Uji-Light
+  ["#fffafc", "#005051", "#95bf43"],	//Uji-Dark
+  ["#1086e3", "#edfafd", "#3ddad7"],	//Amami-Light
+  ["#edfafd", "#1086e3", "#3ddad7"]	//Amami-Dark
 ];
 const Root = document.querySelector(":root");
 
@@ -30,15 +28,14 @@ function themeButton() {
   Button.name = "theme";
   Button.classList.add("theme-button");
   Button.tabIndex = -1;
-	Button.innerText = themeIndex;
-  Button.addEventListener("click", function(){themeChange();}, false);
+  Button.innerText = themeIndex;
+  Button.addEventListener("click", function () { themeChange(); }, false);
   document.querySelector("body").appendChild(Button);
 }
 
 function themeChange() {
-	themeIndex = (themeIndex >= themes.length - 1) ? 0 : themeIndex + 1;
+  themeIndex = (themeIndex >= themes.length - 1) ? 0 : themeIndex + 1;
   themes[themeIndex].forEach((color, index) => Root.style.setProperty(`--color${index + 1}`, color));
-	document.getElementById("themeButton").innerText = themeIndex;
   localStorage.setItem("theme", themeIndex);
   console.log(themeIndex);
 }
